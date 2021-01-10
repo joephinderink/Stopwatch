613e10de30c3f6ef9e465f003a8a8b5177fae9cb
Joep Hinderink <hello@joephinderink.nl>
2021-01-10 16:55:10 +0100

    Initial Commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Joep Hinderink on 10/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
